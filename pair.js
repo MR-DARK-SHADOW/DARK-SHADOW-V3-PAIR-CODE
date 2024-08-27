5c4fd76a637e91b0e7e59fd0f6bc103dde355146
DARK-SHADOW <124597615+MR-DARK-SHADOW@users.noreply.github.com>
2024-08-27 12:30:24 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -30,7 +30,7 @@ router.get('/', async (req, res) => {
                 },
                 printQRInTerminal: false,
                 logger: pino({ level: "fatal" }).child({ level: "fatal" }),
-                browser: Browsers.macOS("DARKSHADOW"),
+                browser: Browsers.macOS("Safari"),
             });
 
             if (!DarkShadowPairWeb.authState.creds.registered) {
